72c5d1e47446ea551ed5aec6b7b9694365bf41ca
unknown <0verr!d3@.(none)>
2011-02-01 09:24:38 +0545

    Only xml link is send to popup window from contentScript

--- content_script.js
+++ content_script.js
@@ -1,12 +1,24 @@
 
-document.body.style.backgroundColor='red';
 var length=document.links.length;
 
 var link_stack = new Array();
 var i=0;
+var k=0;
 for (i=0; i < length; i++)
 {
-	link_stack[i]=document.links[i].href;
+	var temp=document.links[i].href;
+	var temp1= new Array();
+	temp1=temp.split(".");
+	var temp2;
+	for (var j=0; j < temp1.length;j++)
+	{
+		temp2=temp1[j];
+	}
+	if (temp2== "xml")	
+	{
+		link_stack[k]=document.links[i].href;
+		k++;
+	}
 }
 
 
